a0c24a41a3be2592a0dca8d4cf0a3e424d7ad1d8
Piotr Kos <piotrkos99@gmail.com>
2023-01-19 21:10:14 +0100

    Zadanie 1, 2, 3, 4 zrobionED

---
+++ FilmDB/FilmDB/FilmContext.cs
@@ -0,0 +1,16 @@
+﻿using FilmDB.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace FilmDB
+{
+    public class FilmContext : DbContext
+    {
+        public DbSet<FilmModel> Films { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            var cs = @"Data Source=NT-27.WWSI.EDU.PL,1601;Initial Catalog=KASETY_502_10;User ID=Z502_10;Password=********;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False";
+            optionsBuilder.UseSqlServer(cs);
+        }
+    }
+}
